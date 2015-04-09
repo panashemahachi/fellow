351f4630be4643845357856dd90804f54092d1dd
Panashe Mahachi <pkmahach@uwaterloo.ca>
2015-04-08 23:13:22 -0400

    Recreated gemfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409000017) do
+ActiveRecord::Schema.define(version: 20150409031148) do
 
   create_table "artifacts", force: :cascade do |t|
     t.string   "title"
