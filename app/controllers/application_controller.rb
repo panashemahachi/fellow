56bccfe7bcf9e310e51f7ff586920dc038467de5
Panashe Mahachi <pkmahach@uwaterloo.ca>
2015-04-10 02:01:39 -0400

    Changed protect from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+   protect_from_forgery with: :null_session,
+      if: Proc.new { |c| c.request.format =~ %r{application/json} }
+        
   layout :layout_by_resource
   before_action :configure_permitted_parameters, if: :devise_controller?
 
