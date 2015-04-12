618159de8b98d1f67ca44684a33e95fae8da8dd5
Panashe Mahachi <pkmahach@uwaterloo.ca>
2015-04-12 08:45:55 -0400

    Attempted twitter attr fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,8 +44,9 @@ end
 
 def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.twitter_data"] && session["devise.twitter_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+      if session["devise.twitter_data"]
+        #user.email = data["email"] if user.email.blank?
+        user.attributes = params
       end
     end
   end
