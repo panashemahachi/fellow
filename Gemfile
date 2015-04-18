741a006ed3e7bb265252edc3e298821360836e48
Panashe Mahachi <pkmahach@uwaterloo.ca>
2015-04-18 05:31:11 -0400

    Added GA

--- Gemfile
+++ Gemfile
@@ -71,3 +71,5 @@ gem 'activerecord-session_store', github: 'rails/activerecord-session_store'
 gem 'friendly_id'
 
 gem 'will_paginate'
+
+gem 'google-analytics-rails'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  GA.tracker = "UA-61915279-1"
 end
